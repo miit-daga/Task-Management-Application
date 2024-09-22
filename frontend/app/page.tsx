a04e8bbbebd316ca041683fb635f6ba59b5ecf53
Miit Daga <miitcodes27@gmail.com>
2024-09-22 15:39:11 +0530

    Rectified page.tsx in /app

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     const router = useRouter();
 
     useEffect(() => {
-        router.push("/tasklist");
+        router.push("/login");
     }, [router]);
 
     return null;
