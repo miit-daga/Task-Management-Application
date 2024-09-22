627aff1ecdb5c53a84150c84418bf0a588750fb7
Miit Daga <miitcodes27@gmail.com>
2024-09-22 15:36:20 +0530

    Rectified page.tsx in /app

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,18 +6,8 @@ export default function Home() {
     const router = useRouter();
 
     useEffect(() => {
-        const token = getCookie("jwt"); 
-        if (!token) {
-            router.push("/login"); 
-            router.push("/tasklist");
-        }
+        router.push("/tasklist");
     }, [router]);
 
-    return null; 
-}
-
-export function getCookie(name: string): string | undefined {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(';')?.shift();
+    return null;
 }
