255b368da966392c76ac6ac66abc02089f0440a5
Miit Daga <miitcodes27@gmail.com>
2024-09-23 13:03:01 +0530

    test

---
+++ backend (copy)/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const router = Router();
+const authController = require('../controllers/authController');
+const { verifyUser } = require('../middlewares/authMiddleware');
+
+// public routes
+router.post('/signup', authController.createUser);
+router.post('/login', authController.loginUser);
+
+// protected routes
+router.get('/users', verifyUser, authController.getAllUsers);
+router.get('/logout', verifyUser, authController.logoutUser);
+
+module.exports = router;
---
+++ backend (copy)/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const cookieParser = require('cookie-parser');
+
+connectDB();
+const app = express();
+
+const corsOptions = {
+  origin: [
+    "http://localhost:3001", "https://task-management-app-by-miit.vercel.app"
+  ],
+  "Access-Control-Allow-Origin": ["https://task-management-app-by-miit.vercel.app","http://localhost:3001"],
+  "Access-Control-Allow-Credentials": true,
+  methods: "GET,POST,PUT,DELETE,PATCH",
+  optionsSuccessStatus: 200,
+  allowedHeaders: ["Content-Type", "Authorization", "set-cookie"],
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+// Ensure your app handles preflight requests
+app.options('*', cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Welcome to Task Management - api!" });
+});
+
+app.use(authRoutes);
+app.use(taskRoutes); // Ensure that the task routes can be accessed
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`);
+});
